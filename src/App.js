ca04e72a77cae791d7d86583fce707eb3f2ce409
khadiza21 <bibikhadiza474@gmail.com>
2022-03-26 13:30:16 +0600

    header add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return <div className="App">
-    <h1>this is bk</h1>
-    <p>for</p>
+  <Header></Header>
   </div>;
 }
 
