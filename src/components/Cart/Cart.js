fdbea654501ca0f1f258b6a382e7880625522ce1
khadiza21 <bibikhadiza474@gmail.com>
2022-03-26 23:15:04 +0600

    80% done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@
 //here importing nesscessary files and css files
 import React from "react";
 import Dronelist from "../Dronelist/Dronelist";
-import "./Cart.css"
+import "./Cart.css";
 
 //making a custom html tag and also a component
 const Cart = (props) => {
@@ -16,25 +16,30 @@ const Cart = (props) => {
   };
 
   const choose1ForMe = () => {
+    document.getElementById("myDIV").innerHTML = "";
     let index = test(0, cart.length - 1);
     const tem = cart[index];
     alert("Buy " + tem.Name, " , ", "price: " + tem.price);
-    return tem.Name;
+    const para = document.createElement("p");
+    para.innerText =
+      "Luckily You Got " + tem.Name + ". Buy this by " + tem.price + "$";
+    document.getElementById("myDIV").appendChild(para);
+    return para.innerText;
   };
-
 
   return (
     <div className="my-5 py-3 px-2 cart">
       <h5 className="fw-bold">Selected Drones</h5>
       <p className="fw-bold">Selected Items: {props.cart.length}</p>
-      {/* <h6>Total Price: {total}</h6> */}
-       <h6>Ordered Drones Name: </h6>
+      <p id="myDIV" className="fw-bold"></p>
+      <h6>Ordered Drones Name: </h6>
       <ol>
-       
- {cart.map((p) => (
-     <li>     <Dronelist namelist={p} key={p.droneCode}></Dronelist></li>
+        {cart.map((p) => (
+          <li>
+            {" "}
+            <Dronelist namelist={p} key={p.droneCode}></Dronelist>
+          </li>
         ))}
-       
       </ol>
       <button className="btn btn-warning" onClick={choose1ForMe}>
         Choose 1 For Me
